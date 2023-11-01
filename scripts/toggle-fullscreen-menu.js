d1d83606a493dd314ae7afd45aef3cf97019c0eb
lzugaj <luka.zugaj@sigit.hr>
2023-11-01 18:17:57 +0100

    Refactoring

--- scripts/toggle-fullscreen-menu.js
+++ scripts/toggle-fullscreen-menu.js
@@ -1,32 +1,3 @@
-// function handleToggleFullscreenMenu() {
-//   const menuIcon = document.getElementById("menu-icon");
-//   const closeIcon = document.getElementById("close-icon");
-//   const menuElement = document.getElementById("fullscreen-menu");
-
-//   menuIcon.addEventListener("click", () => toggleFullscreenMenu(menuElement));
-//   closeIcon.addEventListener("click", () => closeFullscreenMenu(menuElement));
-// }
-
-// function toggleFullscreenMenu(menuElement) {
-//   menuElement.style.display = "block";
-//   // Use a timeout to ensure the display property change takes effect before applying opacity
-//   setTimeout(() => {
-//     menuElement.style.opacity = "1";
-//   }, 0);
-// }
-
-// function closeFullscreenMenu(menuElement) {
-//   menuElement.style.opacity = "0";
-//   // Listen for the transitionend event to hide the element after the transition
-//   menuElement.addEventListener("transitionend", function () {
-//     if (menuElement.style.opacity === "0") {
-//       menuElement.style.display = "none";
-//     }
-//   });
-// }
-
-// handleToggleFullscreenMenu();
-
 function handleToggleFullscreenMenu() {
   const menuIcon = document.getElementById("menu-icon");
   const closeIcon = document.getElementById("close-icon");
